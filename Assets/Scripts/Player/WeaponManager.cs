be41314ba15e06005749a6a647e9d89322125c19
Marionese <99406835+Marionese@users.noreply.github.com>
2025-11-23 23:28:51 +0100

    Add onAim Method and onAttack

--- Assets/Scripts/Player/WeaponManager.cs
+++ Assets/Scripts/Player/WeaponManager.cs
@@ -6,24 +6,34 @@ public class WeaponManager : MonoBehaviour
     public Transform weaponHolder;
     public GameObject startingWeaponPrefab;
     public WeaponBase currentWeapon; // reference to weapon on holder
-    InputAction shootAction;
+
+    private bool attackPressed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         EquipWeapon(startingWeaponPrefab);
-        shootAction = InputSystem.actions.FindAction("Shoot");
 
     }
+    //input
 
+
+    void OnAim(InputValue val)
+    {
+        if (currentWeapon != null)
+            currentWeapon.SetAim(val.Get<Vector2>());
+    }
+
+    void OnAttack(InputValue val)
+    {
+        attackPressed = val.isPressed;
+    }
     // Update is called once per frame
     void Update()
     {
-        if (shootAction.IsPressed())
+        if (attackPressed)
         {
             currentWeapon.Attack();
-
         }
-
     }
     public void EquipWeapon(GameObject weaponPrefab)
     {
