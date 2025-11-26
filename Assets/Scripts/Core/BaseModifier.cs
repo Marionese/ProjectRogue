eabecb3f439f52e608e5a58d41aaebaea9c90ff3
Marionese <99406835+Marionese@users.noreply.github.com>
2025-11-26 22:32:58 +0100

    Add virtual OnHit in BaseModifier

--- Assets/Scripts/Core/BaseModifier.cs
+++ Assets/Scripts/Core/BaseModifier.cs
@@ -4,6 +4,7 @@ public abstract class BaseModifier : ScriptableObject {}
 
 public abstract class AttackModifier : BaseModifier {
     public abstract void ApplyAttack(ref AttackData data);
+    public virtual void OnHit(EnemyScript enemy, AttackData data) { }
 }
 
 public abstract class PlayerStatModifier : BaseModifier {
