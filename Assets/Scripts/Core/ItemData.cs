690b30da86978da8685c0d75a7e37b590ac40e6d
Marionese <99406835+Marionese@users.noreply.github.com>
2025-11-26 20:37:50 +0100

    add isUnique Flag

--- Assets/Scripts/Core/ItemData.cs
+++ Assets/Scripts/Core/ItemData.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "Items/New Item")]
 public class ItemData : ScriptableObject
-{
+{  
+    public bool isUnique;
     public ItemCategory category;
     public string itemID;
     public string itemName;
