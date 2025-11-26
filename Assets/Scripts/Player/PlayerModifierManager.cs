ecbffcfe7e8e016dea4a3e3d9c34ccebb2cf4c2c
Marionese <99406835+Marionese@users.noreply.github.com>
2025-11-26 22:33:47 +0100

    Add GetAttackModifiers function

--- Assets/Scripts/Player/PlayerModifierManager.cs
+++ Assets/Scripts/Player/PlayerModifierManager.cs
@@ -38,4 +38,15 @@ public class PlayerModifierManager : MonoBehaviour
                 atk.ApplyAttack(ref data);
         }
     }
+    public List<AttackModifier> GetAttackModifiers()
+    {
+        List<AttackModifier> modifiers = new();
+        foreach (var m in activeItems)
+        {
+            if (m is AttackModifier atk)
+                modifiers.Add(atk);
+        }
+
+        return modifiers;
+    }
 }
