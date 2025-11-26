f9ba3951385352e842eaec841c5dcb8d73090761
Marionese <marios.spathiadis@gmail.com>
2025-11-26 15:11:41 +0100

    Add Item Spawnpoint and Chests now spawn 2 items in Coop

--- Assets/Scripts/Interractables/BaseChest.cs
+++ Assets/Scripts/Interractables/BaseChest.cs
@@ -1,10 +1,11 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public abstract class BaseChest : MonoBehaviour, IInteractable
 {
     public GameObject pickupPrefab;
-
+    public Transform itemSpawnPoint;
     protected bool opened = false;
 
     public void Interact(GameObject player)
@@ -19,26 +20,54 @@ public abstract class BaseChest : MonoBehaviour, IInteractable
         }
     }
 
-    protected void SpawnItem(ItemData item)
+    protected void SpawnItem(ItemData item, Vector2 position)
     {
         // Spawn direkt auf der Chest
-        var pickup = Instantiate(pickupPrefab, transform.position, Quaternion.identity);
+        var pickup = Instantiate(pickupPrefab, position, Quaternion.identity);
         pickup.GetComponent<Item>().item = item;
     }
 
     // Child entscheidet über den Pool
-    protected abstract ItemData DecideItem(int playerIndex);
+    protected abstract ItemData DecideItem();
 
     protected virtual void OnChestOpened(int playerIndex)
     {
-        ItemData chosen = DecideItem(playerIndex);
-
-        if (chosen == null)
+        if (!GameSession.Instance.twoPlayer)
         {
-            Debug.Log("No item available for this chest.");
-            return;
-        }
+            ItemData chosen = DecideItem();
 
-        SpawnItem(chosen);
+            if (chosen == null)
+            {
+                Debug.Log("No item available for this chest.");
+                return;
+            }
+
+            SpawnItem(chosen, itemSpawnPoint.position);
+        }
+        else if (GameSession.Instance.twoPlayer)
+        {
+            {
+                ItemData chosen1 = DecideItem();
+                ItemData chosen2 = DecideItem();
+                switch ((chosen1, chosen2))
+                {
+                    case (null, null): 
+                    Debug.Log("No Items Left in Pool");
+                    break;
+                    case (not null,null):
+                    SpawnItem(chosen1,itemSpawnPoint.position);
+                    Debug.Log("No Items Left in Pool for two items");
+                    break;
+                    case (not null,not null): 
+                    SpawnItem(chosen1,itemSpawnPoint.position);
+                    Vector2 temp = itemSpawnPoint.position;
+                    temp.x += 4;
+                    SpawnItem(chosen2,temp);
+                    break;
+                }
+
+
+            }
+        }
     }
 }
