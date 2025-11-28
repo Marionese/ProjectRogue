a9fe2b476235e5b517ae8c54aaa94903cbde2a0f
Marionese <99406835+Marionese@users.noreply.github.com>
2025-11-28 03:52:43 +0100

    Add BulletHitPosition

--- Assets/Scripts/Enemy/EnemyScript.cs
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -5,6 +5,7 @@ public class EnemyScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float maxHealth;
     float currentHealt;
+    public Vector2 BulletHitPosition { get; set; }
     void Start()
     {
         currentHealt = maxHealth;
