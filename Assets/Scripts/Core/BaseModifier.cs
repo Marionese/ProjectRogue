8fadf0f2992236535ffe1865b2ec94df624c0b10
Marionese <99406835+Marionese@users.noreply.github.com>
2025-11-28 02:11:39 +0100

    Add new Priority system to modifiers changing the order they are applied

--- Assets/Scripts/Core/BaseModifier.cs
+++ Assets/Scripts/Core/BaseModifier.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 
-public abstract class BaseModifier : ScriptableObject {}
+public abstract class BaseModifier : ScriptableObject
+{
+    [Tooltip("Lower = applied earlier. Example: Flat bonuses (0), then percent (1), then multipliers (2).")]
+    public int priority = 0;
+}
 
-public abstract class AttackModifier : BaseModifier {
+public abstract class AttackModifier : BaseModifier
+{
     public abstract void ApplyAttack(ref AttackData data);
     public virtual void OnHit(EnemyScript enemy, AttackData data) { }
 }
 
-public abstract class PlayerStatModifier : BaseModifier {
+public abstract class PlayerStatModifier : BaseModifier
+{
     public abstract void ApplyStats(PlayerRuntimeStats stats);
 }
